ab18a015d13f08054fc15a039a7c1738eb71905a
alexcomu <alex.comunian@gmail.com>
2016-04-04 19:56:22 +0200

    aggregated results

--- sum_bandwith_per_day/from_per_slot_to_stats.py
+++ sum_bandwith_per_day/from_per_slot_to_stats.py
@@ -10,11 +10,13 @@ def readable_bytes(num, suffix='B'):
 
 def process(filename):
     with open(filename, "r") as f:
+        result = []
         data = json.load(f)
         days = [i for i in range(1, 30)]
         slots = [i for i in range(8)]
         for day in days:
             if str(day) in data.keys():
+                tmp = []
                 tot_kbytes = 0
                 for slot in slots:
                     if str(slot) in data[str(day)]:
@@ -23,8 +25,13 @@ def process(filename):
                 for slot in slots:
                     if str(slot) in data[str(day)]:
                         print "Slot",slot,"\t->\t",round((float(data[str(day)][str(slot)])*100)/tot_kbytes, 2),"%\t\t",readable_bytes(int(data[str(day)][str(slot)]))
+                        tmp.append({"slot":slot, "perc": round((float(data[str(day)][str(slot)])*100)/tot_kbytes, 2), "bytes":readable_bytes(int(data[str(day)][str(slot)]))})
                     else:
                         print "Slot",slot,"\t->\t0\t\t0 %"
+                        tmp.append({"slot": slot, "perc": 0, "bytes":0})
+                result.append(tmp)
+        with open("aggregated_%s"%filename,"w") as write_f:
+            write_f.write(json.dumps(result))
 
 
 
