a36a070ace03ad6771ae68f1b464c86233039871
alexcomu <alex.comunian@gmail.com>
2016-04-04 17:16:47 +0200

    perday slots

--- sum_bandwith_per_day/mrjob-runner.py
+++ sum_bandwith_per_day/mrjob-runner.py
@@ -1,19 +1,22 @@
 __author__ = 'alexcomu'
-from sum_bandwith_per_day import MRSumBandwithPerDay
+from sum_bandwith_per_day import MRSumBandwithPerDay, MRSumBandwithPerDayPerSlot
 import json, logging
 
 logging.basicConfig(level=logging.INFO)
 
 
-mr_job = MRSumBandwithPerDay()
+mr_job = MRSumBandwithPerDayPerSlot()
 #mr_job.stdin = open('berlinale_csv/berlinale_aggregated.csv')
-mr_job.stdin = open('berlinale_csv/2012_berlinale.csv')
+mr_job.stdin = open('../berlinale_csv/2012_berlinale.csv')
 
 result = {}
 with mr_job.make_runner() as runner:
     runner.run()
     for line in runner.stream_output():
         app_name, kbyte_transf = mr_job.parse_output_line(line)
-        result[app_name] = kbyte_transf
+        day, slot = app_name.split("_")
+        if not day in result:
+            result[day] = {}
+        result[day][slot] = kbyte_transf[:-1]
 
 print json.dumps(result)
