d00fbba010c8911fd56d53f3de971a0625ac530f
alexcomu <alex.comunian@gmail.com>
2016-04-05 11:01:23 +0200

    bandwith per app done

--- sum_bandwith_per_day/slot_per_day/app.js
+++ sum_bandwith_per_day/slot_per_day/app.js
@@ -6,12 +6,29 @@ angular.module("comuapp",[])
     .controller("ComuCtrl", function($scope, $http){
 
         $scope.initPage = function(){
-            $scope.choice = "2012";
+            $scope.year_choice = "2012";
+            $scope.app_choice="ALL";
             $scope.changeChoice();
         };
 
         $scope.changeChoice = function(){
-            $http.get("/result/aggregated_"+$scope.choice+"_perslot.json").success(function(result){
+            $scope.view_message = "Viewing year "+$scope.year_choice;
+            if($scope.app_choice=="ALL"){
+                url = "/result/aggregated_"+$scope.year_choice+"_perslot.json";
+                $scope.view_message+=" for all aggregated apps";
+            }
+            else{
+                url = "/result/aggregated_"+$scope.year_choice+"_result_"+$scope.app_choice+".json";
+                if($scope.app_choice=="rt"){
+                    $scope.view_message += " for RED CARPET";
+                }else if($scope.app_choice=="pk"){
+                    $scope.view_message += " for PRESS CONFERENCE";
+                }else{
+                    $scope.view_message += " for BERLINALE FILM";
+                }
+            }
+
+            $http.get(url).success(function(result){
                 $scope.result = result;
                 $scope.loading = false;
             });
