d15171721a01595e82317c9bd56cd8fc4b71d499
alexcomu <alex.comunian@gmail.com>
2016-04-06 15:00:45 +0200

    users per second

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Data are aggregated with this specific format:
 
-    $ berlinale_film;1454331524;995823735;213.61.32.110;3;1580;0;wowza02-f;_definst_;2010_Heimatland_web.mp4
+    $ berlinale_film;1454331524;995823735;213.61.32.110;a;1580;0;wowza02-f;_definst_;2010_Heimatland_web.mp4
     $ app-name;timestamp-start;session-id;client-IP;durata(sec);kbyte-trasferiti;client-type;nome-server;istanza;nome-stream;
 
 ## How to run
--- users_per_second/mrjob-runner.py
+++ users_per_second/mrjob-runner.py
@@ -9,11 +9,20 @@ mr_job = MREvaluateHour()
 mr_job.stdin = open('../../berlinale_csv/2016_berlinale.csv')
 
 result = {}
+keys = []
+values = []
 with mr_job.make_runner() as runner:
     runner.run()
     for line in runner.stream_output():
         timestamp, occurrences = mr_job.parse_output_line(line)
-        print "%s\t%s" % (timestamp, occurrences)
-        #result[time] = kbyte_transf
+        # print "%s\t%s" % (timestamp, occurrences)
+        result[timestamp] = occurrences
+        keys.append(timestamp)
+        values.append(occurrences)
 
-#print json.dumps(result)
+print json.dumps(result)
+with open("viz/keys.json", "w") as f:
+    f.write(json.dumps(keys))
+with open("viz/values.json", "w") as f:
+    f.write(json.dumps(values))
+
