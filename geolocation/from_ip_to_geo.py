1dded71595755f73dad302abb4e580e5727cbdea
alexcomu <alex.comunian@gmail.com>
2016-04-09 15:30:41 +0200

    geoip - not ready yet

--- geolocation/from_ip_to_geo.py
+++ geolocation/from_ip_to_geo.py
@@ -2,6 +2,8 @@ __author__ = 'alexcomu'
 from mrjob.job import MRJob
 from mrjob.step import MRStep
 
+range_map = {"max": "", "min": "", "file": ""}
+
 
 class MREvaluateGeolocation(MRJob):
 
@@ -23,12 +25,29 @@ class MREvaluateGeolocation(MRJob):
          client_type, server_name, wowza_instance, stream_name) = line.split(";")
         decimal_ip = self.from_ip_to_decimal(client_ip)
 
-        # cerco il range di IP -> Chiamo un API?
-        # controllo se rientra nella valid o nelle other
-        # mappo la country, 1
+        '''
+        Invece che leggere dal giga file, ne faccio tanti piu piccoli e li mappo
+        in modo che se cerco un IP faccio un controllo, sta in un range? se si cerca nel file XYZ
+        Altrimenti guarda il range dopo e cerca di conseguenza
 
-        yield 1, 1
+        Creo un dict con il mapping dei range e con il puntatore al file da usare per cercare
+        '''
 
+        with open("/work/streaming-stats/scripts/geolocation/ip2location.csv", "r") as f:
+            for line in f.readlines():
+                splitted = line.split(",")
+                range1, range2 = splitted[0:2]
+                if decimal_ip > int(range1[1:-1]) and decimal_ip < int(range2[1:-1]):
+                    country = splitted[2][1:-1]
+                    if country in self.valid_region:
+                        yield country, 1
+                    else:
+                        yield "OTHER", 1
+                    break
 
     def sum_aggregation(self, country, occurrences):
         yield country, sum(occurrences)
+
+
+if __name__ == '__main__':
+    MREvaluateGeolocation.run()
