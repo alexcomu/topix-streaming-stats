522270dc90b453cc38816020e317106de1f2f554
alexcomu <alex.comunian@gmail.com>
2016-04-13 16:56:48 +0200

    geolocation done

--- geolocation/from_ip_to_geo.py
+++ geolocation/from_ip_to_geo.py
@@ -1,17 +1,9 @@
 __author__ = 'alexcomu'
 from mrjob.job import MRJob
 from mrjob.step import MRStep
-
-range_map = {"max": "", "min": "", "file": ""}
-
+import requests
 
 class MREvaluateGeolocation(MRJob):
-
-    valid_region = ["IT", "DE", "FR", "GB", "ES", "BE", "NL", "PL", "IE", "DK"]
-
-    def from_ip_to_decimal(self, ip):
-        parts = ip.split('.')
-        return (int(parts[0]) << 24) + (int(parts[1]) << 16) + (int(parts[2]) << 8) + int(parts[3])
 
     def steps(self):
         return [
@@ -23,27 +15,12 @@ class MREvaluateGeolocation(MRJob):
         (app_name, timestamp_start, session_id,
          client_ip, length_stream, kbyte_transf,
          client_type, server_name, wowza_instance, stream_name) = line.split(";")
-        decimal_ip = self.from_ip_to_decimal(client_ip)
+        resp = requests.get("http://geoip.top-ix.org/"+client_ip)
+        if resp.status_code == 200:
+            resp_json = resp.json()
+            if resp_json["status"] == 200:
+                yield resp_json["geoip"]["country_code"], 1
 
-        '''
-        Invece che leggere dal giga file, ne faccio tanti piu piccoli e li mappo
-        in modo che se cerco un IP faccio un controllo, sta in un range? se si cerca nel file XYZ
-        Altrimenti guarda il range dopo e cerca di conseguenza
-
-        Creo un dict con il mapping dei range e con il puntatore al file da usare per cercare
-        '''
-
-        with open("/work/streaming-stats/scripts/geolocation/ip2location.csv", "r") as f:
-            for line in f.readlines():
-                splitted = line.split(",")
-                range1, range2 = splitted[0:2]
-                if decimal_ip > int(range1[1:-1]) and decimal_ip < int(range2[1:-1]):
-                    country = splitted[2][1:-1]
-                    if country in self.valid_region:
-                        yield country, 1
-                    else:
-                        yield "OTHER", 1
-                    break
 
     def sum_aggregation(self, country, occurrences):
         yield country, sum(occurrences)
